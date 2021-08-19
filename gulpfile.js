39d705120b0eff798d6e04f3d1ace088550eb502
AWIRUT RITSAK <52741889+awirr@users.noreply.github.com>
2021-08-19 10:52:19 +0700

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ gulp.task("default", (callback) => {
       serverUrl: "http://localhost:9000",
       token: "", // Add token
       options: {
-        "sonar.projectBaseDir": "/Users/awirut/Documents/AIS/myais-pwa", // Root path project
+        "sonar.projectBaseDir": "", // Root path project
         "sonar.working.directory": "c:/node-gulp-sonarqube/sonar-scaner", // Root forder node-gulp-sonarqube
       },
     },
