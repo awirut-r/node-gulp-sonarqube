ea06ae22f757e0b437763d2a2f51df3cb06fc75e
AWIRUT RITSAK <52741889+awirr@users.noreply.github.com>
2021-08-23 17:24:21 +0700

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,9 @@ gulp.task("default", (callback) => {
       options: {
         "sonar.projectBaseDir": "", // ใส่ Root project ที่เราต้องการ scan เช่น C:/my-project
         "sonar.working.directory": "", // ใส่ Root project node-gulp-sonarqube แล้ว/ด้วย sonar-scaner เช่น C:/node-gulp-sonarqube/sonar-scaner
+        "sonar.issue.ignore.multicriteria": "t1",
+        "sonar.issue.ignore.multicriteria.t1.ruleKey":"typescript:S1871",
+        "sonar.issue.ignore.multicriteria.t1.resourceKey":"**/reducers/**",
       },
     },
     callback
